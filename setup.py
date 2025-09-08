fc9bc07651a75ca9ad03b05455afac8866eed590
eddy0215 <eddy0215@korea.kr>
2025-09-08 13:45:46 +0900

    Fix setup.py installation error

--- setup.py
+++ setup.py
@@ -120,7 +120,7 @@ try:
 	os.system('sudo touch //home/pi/startup.sh')
 	with open("//home/pi/startup.sh",'w') as file_to_write:
 		#file_to_write.write("#!/bin/sh\nsudo python3 " + thisPath + "/server/server.py")
-		file_to_write.write("#!/bin/sh\nsudo python3 " + thisPath + "/server/webServer.py")
+		file_to_write.write("#!/bin/sh\nsudo python3 " + thisPath + "/server/w.py")
 except:
 	pass
 
